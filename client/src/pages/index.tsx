f1871e7d19ee731c6c412b19ddc8b16f1bb22bff
GammaWraith <kahlob.bookout06@gmail.com>
2024-09-02 19:05:40 -0500

    Update index.tsx

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -17,26 +17,6 @@ const IndexPage: NextPage = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <span>
-          <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className={styles.link}
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
       </header>
     </div>
   )
